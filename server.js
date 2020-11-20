81dcd505bdc4018b00974690b28d919c464bf7f8
tuyetau <lutuyetau@gmail.com>
2020-11-20 09:10:12 +0700

    commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const app = express();
+
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+    next();
+  };
+};
+app.use(express.static('./dist/calculator-project'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join('ePortfolio', '/dist/calculator-project/index.html'));
+});
+
+app.use(forceSSL());
+
+app.listen(process.env.PORT || 8080);
